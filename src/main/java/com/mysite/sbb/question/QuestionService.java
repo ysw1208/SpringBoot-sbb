0b73ac42bfb65d34371cfe25baf8f555bed67a82
YSW <tony605a@naver.com>
2024-07-15 11:35:06 +0900

    navbar and pagemove

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
@@ -15,8 +20,11 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 	private final QuestionRepository questionRepository;
 
-	public List<Question> getList() {
-		return this.questionRepository.findAll();
+	public Page<Question> getList(int page) {
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+		return this.questionRepository.findAll(pageable);
 	}
 
 	public Question getQuestion(Integer id) {
@@ -27,9 +35,9 @@ public class QuestionService {
 			throw new DataNotFoundException("question not found");
 		}
 	}
-	
-	public void create(String subject,String content) {
-		Question q=new Question();
+
+	public void create(String subject, String content) {
+		Question q = new Question();
 		q.setSubject(subject);
 		q.setContent(content);
 		q.setCreateDate(LocalDateTime.now());
